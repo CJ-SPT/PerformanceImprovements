6c92a076e9f21805a32502f752519d15882186c2
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-12-09 19:25:07 -0500

    refactor

--- PerformanceImprovements/Patches/LocalizationPatches.cs
+++ PerformanceImprovements/Patches/LocalizationPatches.cs
@@ -21,3 +21,19 @@ public class AddLocalizationsPatch : ModulePatch
         __instance.method_2(localeId, __instance.method_3(json));
     }
 }
+
+public class AddMainMenuLocalizationsPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return AccessTools.Method(typeof(LocaleManagerClass), nameof(LocaleManagerClass.UpdateMainMenuLocales));
+    }
+
+    [PatchPrefix]
+    public static void PatchPrefix(LocaleManagerClass __instance, string localeId, Dictionary<string, string> newLocale)
+    {
+        var json = EmbededResourceUtil.GetEmbededLocalizationJson();
+        
+        __instance.method_2(localeId, __instance.method_3(json));
+    }
+}
