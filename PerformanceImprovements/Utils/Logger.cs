65e0412b8d0905ac836b1d6bd425c72016ffb958
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-12-14 11:49:13 -0500

    Add caller to logging

--- PerformanceImprovements/Utils/Logger.cs
+++ PerformanceImprovements/Utils/Logger.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx.Logging;
+﻿using System.Diagnostics;
+using BepInEx.Logging;
 
 namespace PerformanceImprovements.Utils;
 
@@ -16,26 +17,42 @@ public class Logger
     {
         if (!_debug) return;
         
-        _logger.LogDebug(message);
+        _logger.LogDebug($"{GetCallersName()} :: {message}");
     }
     
     public static void Info(string message)
     {
-        _logger.LogInfo(message);
+        _logger.LogInfo(BuildMessage(message));
     }
     
     public static void Warn(string message)
     {
-        _logger.LogWarning(message);
+        _logger.LogWarning(BuildMessage(message));
     }
     
     public static void Error(string message)
     {
-        _logger.LogError(message);
+        _logger.LogError(BuildMessage(message));
     }
     
     public static void Fatal(string message)
     {
-        _logger.LogFatal(message);
+        _logger.LogFatal(BuildMessage(message));
+    }
+
+    private static string BuildMessage(string message)
+    {
+        message = _debug 
+            ? $"{GetCallersName()} :: {message}"
+            : message;
+        
+        return message;
+    }
+    
+    private static string GetCallersName()
+    {
+        var stackTrace = new StackTrace();
+        var methodBase = stackTrace.GetFrame(3).GetMethod();
+        return $"{methodBase.DeclaringType?.Name}::{methodBase.Name}()"; 
     }
 }
